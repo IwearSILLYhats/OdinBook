d50350acbdbf78e1e0f12446c1afafbba3cf0c38
Austin <austin.moorer@gmail.com>
2026-02-07 15:33:03 -0500

    Corrected naming convention for Login and Signup pages

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { useNavigate, NavLink } from "react-router";
+import apiFetch from "../../api/api";
+
+export default function Signup() {
+  const [submitted, setSubmitted] = useState(false);
+  const [email, setEmail] = useState(null);
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirm, setConfirm] = useState(null);
+  const [validation, SetValidation] = useState(null);
+  const navigate = useNavigate();
+
+  function signupSubmit(event) {
+    event.preventDefault();
+    setSubmitted(true);
+    const body = {
+      email,
+      username,
+      password,
+      confirm,
+    };
+    const request = apiFetch("signup", "POST", body);
+  }
+
+  return (
+    <div>
+      <form action="" method="post">
+        <label htmlFor="email">Email address</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="username"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="confirm">Confirm Password</label>
+        <input
+          type="password"
+          name="confirm"
+          id="confirm"
+          placeholder="confirm password"
+          required
+          onChange={(e) => setConfirm(e.target.value)}
+        />
+        <button type="submit" disabled={submitted}>
+          Submit
+        </button>
+      </form>
+      <button disabled="disabled">Sign in with Google</button>
+      <p>
+        Already have an account? <NavLink to={"/login"}>Log in here</NavLink>
+      </p>
+    </div>
+  );
+}
