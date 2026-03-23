c6de08a6d404cf0f9a345433067f173fe6451a25
Austin <austin.moorer@gmail.com>
2026-03-23 18:29:18 -0400

    updated schema to add unpublished post/draft functionality

---
+++ backend/prisma/migrations/20260323200410_added_publishing_to_post_schema/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `published` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "published" BOOLEAN NOT NULL,
+ADD COLUMN     "published_time" TIMESTAMP(3);
