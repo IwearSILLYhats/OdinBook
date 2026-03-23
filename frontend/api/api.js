461b415090fbd80cda34545de8d62f49cb516bd5
Austin <austin.moorer@gmail.com>
2026-03-23 18:33:22 -0400

    Error handling for api requests

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,18 +1,22 @@
 const url = import.meta.env.VITE_API_URL;
 
 export default async function apiFetch(endpoint, method, body) {
-  const reqUrl = `${url}/${endpoint}`;
-  const content = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: method,
-    credentials: "include",
-  };
-  if (body !== null) {
-    content.body = JSON.stringify(body);
+  try {
+    const reqUrl = `${url}/${endpoint}`;
+    const content = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: method,
+      credentials: "include",
+    };
+    if (body !== null) {
+      content.body = JSON.stringify(body);
+    }
+    const request = await fetch(reqUrl, content);
+    const response = await request.json();
+    return response;
+  } catch (error) {
+    console.log(error);
   }
-  const request = await fetch(reqUrl, content);
-  const response = await request.json();
-  return response;
 }
